7290bda3cb604a70f1498a7f81ea9ea1e936eb67
imdshk <83509028+imdshk@users.noreply.github.com>
2024-02-24 13:34:46 +0200

    exercise 5.7 completed

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [detailsVisibile, setDetailsVisible] = useState(false)
+
+  const blogStyle = {
+    color: "black",
+    background: "lightgray",
+    fontSize: 20,
+    padding: 10,
+    borderStyle: "solid",
+    borderRadius: 5,
+    BorderColor: "black",
+    marginBottom: 10
+  }
+
+  const showDetails = { display: detailsVisibile ? "" : "none" }
+
+  const toggleDetailsVisibility = () => {
+    setDetailsVisible(!detailsVisibile)
+  }
+
+  return(
+  <div style={blogStyle}>
     {blog.title} {blog.author}
+    <button onClick={toggleDetailsVisibility}>{detailsVisibile ? "hide" : "view"}</button>
+    <div style={showDetails}>
+      {blog.url}
+      <br/>
+      likes {blog.likes}
+      <button>like</button>
+      <br/>
+      {blog.user.name}
+    </div>
   </div>  
-)
+)}
 
 export default Blog
